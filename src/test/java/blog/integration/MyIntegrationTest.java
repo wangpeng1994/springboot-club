9156e16cb46423b26c82ba414a3bb0a897151890
wangpeng1994 <947034046@qq.com>
2020-06-17 22:19:35 +0800

    引入flyway，添加创建user表的语句；配置一条集成测试

---
+++ src/test/java/blog/integration/MyIntegrationTest.java
@@ -0,0 +1,12 @@
+package blog.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class MyIntegrationTest {
+
+    @Test
+    void foo() {
+        System.out.println("------------- 喵喵喵 -----------");
+    }
+
+}
