fb44a8f92d98e20d659160ad7b4a5c5db1cb2e11
wangpeng1994 <947034046@qq.com>
2020-06-20 00:11:53 +0800

    add @Service annotation for BlogDao.

--- src/main/java/blog/dao/BlogDao.java
+++ src/main/java/blog/dao/BlogDao.java
@@ -1,9 +1,11 @@
 package blog.dao;
 
 import blog.entity.Blog;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BlogDao {
 
     public List<Blog> getBlogs(Integer page, Integer pageSize, Integer userId) {
